ac6f450361c8c34df8a87f19e53ed60e0508cef7
selimyildiz <94224409+iamselimyildiz@users.noreply.github.com>
2023-06-18 16:21:25 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,29 @@
+from pyfirmata import Arduino,SERVO,util
+from time import sleep
+
+port= "COM3"
+pin = 10
+board= Arduino(port)
+
+board.digital[pin].mode=SERVO
+
+def cevir_servo(pin,angle):
+    board.digital[pin].write(angle)
+    sleep(0.001)
+
+
+while True:
+    x=input('sayı giriniz: ')   
+
+    if x == "1":
+        for i in range(0,45):
+            cevir_servo(pin,i)
+
+    elif x=='2':        
+        for i in range(0,90):
+            cevir_servo(pin,i)
+
+    elif x=='3':
+        for i in range(0,180):
+            cevir_servo(pin,i)
+
